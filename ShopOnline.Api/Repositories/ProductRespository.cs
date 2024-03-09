89e1aa7f691e573869f1a05afbe5e3ab9957aa88
Steven Wu <ZhuohengWu@users.noreply.github.com>
2024-03-09 11:34:23 -0500

    Add product repository

---
+++ ShopOnline.Api/Repositories/ProductRespository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRespository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext shopOnlineDbContext;
+
+        public ProductRespository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.shopOnlineDbContext = shopOnlineDbContext;
+        }
+
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await shopOnlineDbContext.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await shopOnlineDbContext.Products.ToListAsync();
+            return products;
+        }
+    }
+}
