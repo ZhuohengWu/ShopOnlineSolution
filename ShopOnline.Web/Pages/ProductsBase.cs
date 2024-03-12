63daf9a7d0c5d85d229d0a715d969bbba20cb78a
Steven Wu <ZhuohengWu@users.noreply.github.com>
2024-03-12 16:33:02 -0400

    Display products to user

--- ShopOnline.Web/Pages/ProductsBase.cs
+++ ShopOnline.Web/Pages/ProductsBase.cs
@@ -7,13 +7,32 @@ namespace ShopOnline.Web.Pages
     public class ProductsBase: ComponentBase
     {
         [Inject]
-        public IProductService ProductService { get; set; }
-        public IEnumerable<ProductDto> Products { get; set; }
+        public IProductService? ProductService { get; set; }
+        public IEnumerable<ProductDto>? Products { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            Products = await ProductService.GetItems();
+            if (ProductService is not null) 
+            {
+                Products = await ProductService.GetItems();
+            }
+        }
+        protected IOrderedEnumerable<IGrouping<int, ProductDto>> GetGroupedProductsByCategory()
+        {
+            return from product in Products
+                   group product by product.CategoryId into prodByCatGroup
+                   orderby prodByCatGroup.Key
+                   select prodByCatGroup;
+        }
+        protected string GetCategoryName(IGrouping<int, ProductDto> groupedProductDto)
+        {
+            var product = groupedProductDto.FirstOrDefault(pg => pg.CategoryId == groupedProductDto.Key);
 
+            if (product is not null)
+            {
+                return product.CategoryName;
+            }
+            return string.Empty;
         }
     }
 }
